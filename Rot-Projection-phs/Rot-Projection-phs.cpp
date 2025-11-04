4c5c29de0567da4bcc4da35171b9e7b55f0fb8c2
Paul-Henri-Solat-Gtech <psolat@gaming.tech>
2025-11-04 08:57:33 +0100

    1.2

--- Rot-Projection-phs/Rot-Projection-phs.cpp
+++ Rot-Projection-phs/Rot-Projection-phs.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <Windows.h>
 
 int main(int argc, char** argv)
 {
-    std::cout << "Hello World\n";
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD mode;
+    GetConsoleMode(hConsole, &mode);
+    SetConsoleMode(hConsole, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+
+    std::string ansi_clear = "\033[2J";
+    std::string ansi_firstPos = "\033[H";
+    std::string ansi_hideCursor = "\033[?25l";
+    std::string ansi_showCursor = "\033[? 25h";
+
+    std::cout << "Hello World (this text will be cleared)\n";
+
+    printf(ansi_clear.c_str());
+    printf(ansi_firstPos.c_str());
+    printf(ansi_hideCursor.c_str());
+
+    std::cout << "Haaa (this text will be in first)\n";
+
     return 0;
 }
